640b8dbab0e5e06d50a81de6f810f3950a863a99
netdiscada <menino.belu90@gmail.com>
2026-02-21 13:23:10 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Instalado');
-});
-
-self.addEventListener('fetch', (e) => {
-    // Permite que o app funcione abrindo o navegador internamente
-    e.respondWith(fetch(e.request));
-});
